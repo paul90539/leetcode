ad1f3e97cde936f5011462073c0911001c76747a
LaLaKo <paul90539@gmail.com>
2021-08-05 01:26:22 +0800

    Update LongestSubstringWithoutRepeatingCharacters.cpp

--- algorithms/cpp/3. Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.cpp
+++ algorithms/cpp/3. Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.cpp
@@ -5,12 +5,15 @@ public:
         int sLength = s.size(), resultLength = 0, currLength = 0;
         char checkTable[127] = {0}, cBit, headBit;
         
+        // walk full string, O(n)
         for(int index = 0; index < sLength; index++){            
             cBit = s[index];
             
+            // if character repeat, remove front character
             if(checkTable[cBit] == 1){
                 resultLength = currLength > resultLength ? currLength : resultLength;
                 
+                //O(n)
                 while( currLength > 0){
                     headBit = charQueue.front();
                     charQueue.pop();
